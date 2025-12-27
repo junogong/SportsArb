bd471ffc71261d0af26b55111e46fb08ec705f6b
juno <gongju904@gmail.com>
2025-12-27 08:15:51 -0500

    added detection for local host editing or on live web to redirect url

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,28 @@ import './styles.css'
 import { Amplify } from 'aws-amplify'
 import outputs from './amplifyconfiguration.json'
 import '@aws-amplify/ui-react/styles.css'
-Amplify.configure(outputs)
+
+// Check if running locally
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "[::1]" ||
+  window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+);
+
+// Set redirect URL based on environment
+const redirectUrl = isLocalhost ? 'http://localhost:5173/' : 'https://www.stakt.live/';
+
+// Update config
+const updatedConfig = {
+  ...outputs,
+  oauth: {
+    ...outputs.oauth,
+    redirectSignIn: redirectUrl,
+    redirectSignOut: redirectUrl,
+  }
+}
+
+Amplify.configure(updatedConfig)
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
